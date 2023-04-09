5d39eda96ceca57e59bd04ec9238bee17c11334f
v05t <130287698+v05t@users.noreply.github.com>
2023-04-09 17:27:34 +0500

    Update proect.py

--- proect.py
+++ proect.py
@@ -1,10 +1,26 @@
-print('hello, это проект'
-      or i range('red')
-pint('пока)
-     a = weapon(
-     b = input(in('my name')
-c = int(iput('my surname'
-print('buy)
-             for  range(black')
-v  hp()
- = bow()             
+from PyQt5.QtWidgets import*
+
+app = QApplication([])
+okno = QWidget()
+
+main = QHBoxLayout()
+okno.setLayout(main)
+
+line1 = QVBoxLayout()
+line2 = QVBoxLayout()
+main.addLayout(line1)
+main.addLayout(line2)
+
+b1 = QPushButton('Дальше')
+tx1 = QLabel('Результат')
+line1.addWidget(b1)
+line2.addWidget(tx1)
+tx1.hide()
+
+def next1():
+    tx1.show()
+    b1.hide()
+b1.clicked.connect(next1)
+
+okno.show()
+app.exec_()         
