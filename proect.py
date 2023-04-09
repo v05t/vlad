bef164294ff712580f1b772e07037c50b65780d9
v05t <130287698+v05t@users.noreply.github.com>
2023-04-09 16:52:09 +0500

    Create proect.py

---
+++ proect.py
@@ -0,0 +1,10 @@
+print('hello, это проект'
+      or i range('red')
+pint('пока)
+     a = weapon(
+     b = input(in('my name')
+c = int(iput('my surname'
+print('buy)
+             for  range(black')
+v  hp()
+ = bow()             
