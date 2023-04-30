79556d945fd0bc8a0dc199898ef288034cd055fa
v05t <130287698+v05t@users.noreply.github.com>
2023-04-30 16:05:12 +0500

    Update proect.py

--- proect.py
+++ proect.py
@@ -3,24 +3,86 @@ from PyQt5.QtWidgets import*
 app = QApplication([])
 okno = QWidget()
 
-line1 = QVBoxLayout()
-okno.setLayout(line1)
-
-text1 = QLabel('Нажми на кнопку')
-line1.addWidget(text1)
-text1.setStyleSheet('QLabel{font-size:100px; color: red;}')
-
-but1 = QPushButton('Кнопка')
-line1.addWidget(but1)
-but1.setStyleSheet('''QPushButton{font-size:50px; 
-background: qlineargradient(x1:0 y1:0, x2:1 y2:0, stop:0 red, stop:1 blue);
- color: yellow
-}''')
-okno.setStyleSheet('''QWidget{background-image: url("Shrek1.jpg");
-}''')
-
+mline = QHBoxLayout()
+okno.setLayout(mline)
 line2 = QVBoxLayout()
-line1.addLayout(line2)
+mline.addLayout(line2)
+
+vvod1 = QLineEdit()
+vvod2 = QLineEdit()
+line2.addWidget(vvod1)
+line2.addWidget(vvod2)
+
+
+line3 = QVBoxLayout()
+mline.addLayout(line3)
+
+
+plus = QPushButton('+')
+line3.addWidget(plus)
+
+minus = QPushButton('-')
+line3.addWidget(minus)
+
+multiply = QPushButton('*')
+line3.addWidget(multiply)
+
+divide = QPushButton(':')
+line3.addWidget(divide)
+
+
+result = QLabel('?')
+mline.addWidget(result)
+result.setStyleSheet('QLabel{font-size:50px}')
+
+def pribav():
+    try:
+        a = int(vvod1.text())
+        b = int(vvod2.text())
+        res = a + b
+        result.setText(str(res))
+    except:
+        okno2 = QMessageBox()
+        okno2.setText('Только числа!')
+        okno2.exec_()
+
+plus.clicked.connect(pribav)
+
+def subtract():
+    try:
+        a = int(vvod1.text())
+        b = int(vvod2.text())
+        res2 = a - b
+        result.setText(str(res2))
+    except:
+        okno2 = QMessageBox()
+        okno2.setText('Только числа!')
+        okno2.exec_()
+minus.clicked.connect(subtract)
+
+def delet():
+    try:
+        a = int(vvod1.text())
+        b = int(vvod2.text())
+        res2 = a / b
+        result.setText(str(res2))
+    except:
+        okno2 = QMessageBox()
+        okno2.setText('Только числа!')
+        okno2.exec_()
+divide.clicked.connect(delet)
+
+def ymnow():
+    try:
+        a = int(vvod1.text())
+        b = int(vvod2.text())
+        res2 = a * b
+        result.setText(str(res2))
+    except:
+        okno2 = QMessageBox()
+        okno2.setText('Только числа!')
+        okno2.exec_()
+multiply.clicked.connect(ymnow)
 
 okno.show()
 app.exec_()
